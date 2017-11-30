b5ccbd684de21eba9e3a6a8482c1ee71130d0b11
victorglimskog <victor.glimskog@gmail.com>
2017-11-30 15:27:19 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const pm = require('promisemaker');
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+
+// Create a connection
+const db = pm(
+  mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "myskul",
+    database: "petregister"
+  }),
+  {
+    rejectOnErrors: false,
+    mapArgsToProps: {
+      query: ["rows", "fields"]
+    }
+  }
+);
+
+app.get('/petowners', async function(req,res) {
+    let result = await query('SELECT * FROM petowners');
+    console.log(result);
+    res.json(result);
+});
+
+app.get('/pets/:pnr', async function(req,res) {
+    const pnr = req.params.pnr;
+    console.log(pnr);
+    let result = await query('SELECT * FROM petsandowners WHERE pnr = ' + pnr);
+    res.json(result);
+});
+
+app.listen(3000, () => {
+    console.log('app listening on port 3000');
+});
+
+// a helper function that makes query syntax shorter
+async function query(query, params){
+    let result = await db.query(query, params);
+    return result.rows;
+}
