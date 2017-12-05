3c877b890b12df6127d5dc5b2c548b874054c577
victorglimskog <victor.glimskog@gmail.com>
2017-12-05 13:13:25 +0100

    fix

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: __dirname + '/.env'});
+require('dotenv').config();
 const pm = require('promisemaker');
 const mysql = require('mysql');
 const express = require('express');
